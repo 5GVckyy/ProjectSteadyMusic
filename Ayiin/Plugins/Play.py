b0dc620311de415fa693773b3ac26bde6c1dfc62
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-08-25 15:27:29 +0700

    Update Play.py

--- Ayiin/Plugins/Play.py
+++ Ayiin/Plugins/Play.py
@@ -200,7 +200,8 @@ async def play(_, message: Message):
         )
         await CallbackQuery.edit_message_text(
             f"1️⃣<b>{results[0]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[0]['id']})__</u>\n\n2️⃣<b>{results[1]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[1]['id']})__</u>\n\n3️⃣<b>{results[2]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[2]['id']})__</u>\n\n4️⃣<b>{results[3]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[3]['id']})__</u>\n\n5️⃣<b>{results[4]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[4]['id']})__</u>",
-            reply_markup=InlineKeyboardMarkup(buttons)
+            reply_markup=InlineKeyboardMarkup(buttons),
+        )
 
 
 @app.on_callback_query(filters.regex(pattern=r"MusicStream"))
