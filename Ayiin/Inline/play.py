de899413ec582edb3ebeb260be6d4a3a6eff8155
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-08-25 10:08:22 +0700

    Update play.py

--- Ayiin/Inline/play.py
+++ Ayiin/Inline/play.py
@@ -12,7 +12,7 @@ def url_markup(videoid, duration, user_id, query, query_type):
                 callback_data=f"slider B|{query_type}|{query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🎵",
+                text="Play Music",
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
