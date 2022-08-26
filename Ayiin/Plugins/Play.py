0033d55d1691877079ad2e8ddc4fcbc36eaa5471
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-08-26 22:56:55 +0700

    Update Play.py

--- Ayiin/Plugins/Play.py
+++ Ayiin/Plugins/Play.py
@@ -176,55 +176,23 @@ async def play(_, message: Message):
             return
         mystic = await message.reply_text("🔍 **Searching**...")
         query = message.text.split(None, 1)[1]
-    med = InputMediaPhoto(
-        media="Utils/ayiin.jpg",
-        caption=(
-            f"1️⃣<b>{results[0]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[0]['id']})__</u>\n\n2️⃣<b>{results[1]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[1]['id']})__</u>\n\n3️⃣<b>{results[2]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[2]['id']})__</u>\n\n4️⃣<b>{results[3]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[3]['id']})__</u>\n\n5️⃣<b>{results[4]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[4]['id']})__</u>"
-        ),
-    )
-        try:
-        results = YoutubeSearch(query, max_results=5).to_dict()
-        buttons = search_markup2(
-            results[5]["id"],
-            results[6]["id"],
-            results[7]["id"],
-            results[8]["id"],
-            results[9]["id"],
-            results[5]["duration"],
-            results[6]["duration"],
-            results[7]["duration"],
-            results[8]["duration"],
-            results[9]["duration"],
-            user_id,
-            query,
+        (
+            title,
+            duration_min,
+            duration_sec,
+            thumb,
+            videoid,
+        ) = get_yt_info_query(query)
+        await mystic.delete()
+        buttons = url_markup(
+            videoid, duration_min, message.from_user.id, query, 0
         )
-        await CallbackQuery.edit_message_text(
-            f"6️⃣<b>{results[5]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[5]['id']})__</u>\n\n7️⃣<b>{results[6]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[6]['id']})__</u>\n\n8️⃣<b>{results[7]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[7]['id']})__</u>\n\n9️⃣<b>{results[8]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[8]['id']})__</u>\n\n🔟<b>{results[9]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[9]['id']})__</u>",
+        return await message.reply_photo(
+            photo=thumb,
+            caption=f"📎Title: **{title}\n\n⏳Duration:** {duration_min} Mins\n\n__[Get Additional Information About Video](https://t.me/{BOT_USERNAME}?start=info_{videoid})__",
             reply_markup=InlineKeyboardMarkup(buttons),
         )
-        disable_web_page_preview = True
-        return
-    if int(i) == 2:
-        buttons = search_markup(
-            results[0]["id"],
-            results[1]["id"],
-            results[2]["id"],
-            results[3]["id"],
-            results[4]["id"],
-            results[0]["duration"],
-            results[1]["duration"],
-            results[2]["duration"],
-            results[3]["duration"],
-            results[4]["duration"],
-            user_id,
-            query,
-        )
-        await CallbackQuery.edit_message_text(
-            f"1️⃣<b>{results[0]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[0]['id']})__</u>\n\n2️⃣<b>{results[1]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[1]['id']})__</u>\n\n3️⃣<b>{results[2]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[2]['id']})__</u>\n\n4️⃣<b>{results[3]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[3]['id']})__</u>\n\n5️⃣<b>{results[4]['title']}</b>\n  ┗  🔗 <u>__[Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{results[4]['id']})__</u>",
-            reply_markup=InlineKeyboardMarkup(buttons),
-        )
-        disable_web_page_preview = True
-        return
+
 
 @app.on_callback_query(filters.regex(pattern=r"MusicStream"))
 async def Music_Stream(_, CallbackQuery):
